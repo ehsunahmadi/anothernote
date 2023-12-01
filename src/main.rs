e76623e3682a0d210c4120372ea031b82f98ccbc
ehsun ahmadi <ehsun.ahmandi@gmail.com>
2023-12-02 00:23:14 +0330

    use a build func outside of main

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,25 @@ use std::path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-    let name = format!("{}.md", filename);
-    let path = path::Path::new(&name);
-    let duplicate = path.exists();
-    if duplicate {
-        eprintln!("{} already exists", path.display());
-    }
-    fs::File::create(path).unwrap_or_else(|why| {
-        eprintln!("couldn't create {}: {}", filename, why);
+    let p = build(&args).unwrap_or_else(|why| {
+        eprintln!("Error: {}", why);
         std::process::exit(1);
     });
+    fs::File::create(&p).unwrap_or_else(|why| {
+        eprintln!("Error: couldn't create {}: {}", p.display(), why);
+        std::process::exit(1);
+    });
+}
+
+fn build(args: &[String]) -> Result<path::PathBuf, &'static str> {
+    if args.len() < 2 {
+        return Err("please provide a filename");
+    }
+    let filename = args[1].clone() + ".md";
+    let p = path::PathBuf::from(filename);
+    let duplicate = p.exists();
+    if duplicate {
+        return Err("file already exists");
+    }
+    Ok(p)
 }
