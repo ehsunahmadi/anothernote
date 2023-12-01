025e05d9f40e964c00820f1ad5546a5ed9d17846
ehsun ahmadi <ehsun.ahmandi@gmail.com>
2023-12-01 22:03:32 +0330

    working in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::fs;
+use std::path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let name = format!("{}.md", filename);
+    let path = path::Path::new(&name);
+    let duplicate = path.exists();
+    if duplicate {
+        eprintln!("{} already exists", path.display());
+    }
+    fs::File::create(path).unwrap_or_else(|why| {
+        eprintln!("couldn't create {}: {}", filename, why);
+        std::process::exit(1);
+    });
 }
